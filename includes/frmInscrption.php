3aa232408535980e3fde63a0cddb081de3d790ad
Axel Barbé--Husson <barbehussonaxel@gmail.com>
2019-02-20 09:55:25 +0100

    add includes

---
+++ includes/frmInscrption.php
@@ -0,0 +1,26 @@
+<?php
+if (!isset($nom)) $nom = "";
+if (!isset($mail)) $mail = "";
+?>
+
+<form method="post" action="index.php?page=inscription">
+    <div>
+        <label for="nom">Nom&nbsp;: </label>
+        <input type="text" id="nom" name="nom" />
+        <input type="text" id="nom" name="nom" value="<?=$nom?>"/>
+    </div>
+    <div>
+        <label for="mail">Mail&nbsp;: </label>
+        <input type="text" id="mail" name="mail" />
+        <input type="text" id="mail" name="mail" value="<?=$mail?>" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe&nbsp;: </label>
+        <input type="password" id="mdp" name="mdp" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+    <input type="hidden" name="maurice" />
+</form>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?page=accueil">Accueil</a></li>
+            <li><a href="index.php?page=produits">Produits</a></li>
+            <li><a href="index.php?page=inscription">Inscription</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
